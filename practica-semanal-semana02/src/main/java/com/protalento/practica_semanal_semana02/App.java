27fc0ace218d3f598c1db097b46e16685b58d8df
Alex_Nieto0209 <alexcreativo0209@gmail.com>
2022-05-30 12:06:10 -0500

    se creo el primer archivo

---
+++ practica-semanal-semana02/src/main/java/com/protalento/practica_semanal_semana02/App.java
@@ -0,0 +1,13 @@
+package com.protalento.practica_semanal_semana02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ practica-semanal-semana02/src/test/java/com/protalento/practica_semanal_semana02/AppTest.java
@@ -0,0 +1,20 @@
+package com.protalento.practica_semanal_semana02;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
